0d635b5ec045771b2c5c65e0bba895f2595d58cf
Const679 <116187678+Const679@users.noreply.github.com>
2022-11-30 20:25:08 +0100

    django2

--- django/diabetes/polls/views.py
+++ django/diabetes/polls/views.py
@@ -1,4 +1,22 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as snb
+from sklearn.preprocessing import LabelEncoder
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index")
+    template=loader.get_template('template0.html')
+    df= pd.read_csv('diabetic_data.csv', delimiter=',')
+    nRow, nCol = df.shape
+    plt.figure(figsize=(16,8))
+    corr_matrix = df.corr()
+    snb.heatmap(corr_matrix)
+    plot_html=plt.to_html(full_html=False,default_height=500)
+    context={'leprint':f'There are {nRow} rows and {nCol} columns',
+    'plot_html':plot_html}
+    return HttpResponse(template.render(context,request))
