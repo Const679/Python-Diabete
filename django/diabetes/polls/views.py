bd77ef15b80044d94aecc750eb9532b592f836ac
Const679 <116187678+Const679@users.noreply.github.com>
2022-12-01 12:57:58 +0100

    fifth

--- django/diabetes/polls/views.py
+++ django/diabetes/polls/views.py
@@ -13,10 +13,10 @@ def index(request):
     template=loader.get_template('template0.html')
     df= pd.read_csv('diabetic_data.csv', delimiter=',')
     nRow, nCol = df.shape
-    plt.figure(figsize=(16,8))
+    fig=plt.figure(figsize=(16,8))
     corr_matrix = df.corr()
     snb.heatmap(corr_matrix)
-    plot_html=plt.to_html(full_html=False,default_height=500)
+    plot_html=fig.to_html(full_html=False,default_height=500)
     context={'leprint':f'There are {nRow} rows and {nCol} columns',
     'plot_html':plot_html}
     return HttpResponse(template.render(context,request))
